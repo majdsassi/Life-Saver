8b55ba866848867e41c618569006e0bdf2dae51d
majdsassi <jackreider997@gmail.com>
2025-11-09 23:35:33 +0100

    Fix Logout Path

--- medecin/includes/header.php
+++ medecin/includes/header.php
@@ -11,7 +11,7 @@
         <a class="navbar-brand" href="index.php">Life Saver - Admin</a>
         <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
             <li class="nav-item">
-                <a href="http://localhost/Life-Saver/admin/utils/logout.php" class="btn btn-outline-light btn-sm">
+                <a href="../utils/logout.php" class="btn btn-outline-light btn-sm">
                     <i class="bi bi-box-arrow-right"></i> Déconnexion
                 </a>
             </li>
