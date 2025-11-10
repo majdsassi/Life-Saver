4f0d9d50974973e3bd4f30145dea0ca2c0f87b1f
majdsassi <jackreider997@gmail.com>
2025-11-10 01:09:53 +0100

    Fix Find Button

--- includes/hero.php
+++ includes/hero.php
@@ -2,7 +2,7 @@
         <div class="container">
             <h1>Donate Blood, Save Lives</h1>
             <p>Your single donation can save up to three lives. Join thousands of donors who are making a difference in their communities every day.</p>
-            <a href="#donate" class="btn btn-primary btn-lg me-2">Find a Center </a>
+            <a href="centres.php" class="btn btn-primary btn-lg me-2">Find a Center </a>
             <a href="#find-center" class="btn btn-outline-light btn-lg">Contact us </a>
         </div>
 </section>
