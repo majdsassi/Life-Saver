732023818a38c8d0712174aaa8cb2cc78b371ba9
majdsassi <jackreider997@gmail.com>
2025-11-19 11:02:09 +0100

    Fix Conflict

---
+++ handlers/clinetLoginHandler.php
@@ -0,0 +1,41 @@
+<?php
+require_once '../config.php';
+require_once '../utils/connection.php' ; 
+session_start();
+
+if (!empty($_POST)) {
+    if (isset($_POST['username']) && isset($_POST['password'])) {
+        try {
+            
+            $stmt = $pdo->prepare("SELECT * FROM clients WHERE cin = ?"); // Protection Contre les SQL injections hekka 3lh n7oto ?
+            $stmt->execute([$_POST['username']]);
+            $user = $stmt->fetch(PDO::FETCH_ASSOC);
+            
+            // Verify user password and set SESSION
+            if ($user && password_verify($_POST['password'], $user["password"])) {
+                $_SESSION['user_id'] = $user["id_donneur"];
+                header("Location:".DOMAIN."/client/index.php");
+                exit;
+            } else {
+                // Creds Ghalta 
+               header("Location:".DOMAIN."clinetLogin.php?error=invalid_credentials");
+                exit;
+            }
+            
+        } catch (PDOException $e) {
+            header("Location:".DOMAIN."clinetLogin.php?error=database_error");
+            //echo "ERROR BASE DE DONNNE" ;
+            error_log("Database error: " . $e->getMessage());
+            exit;
+        }
+    } else {
+        //  username wa ela  password ne9sa
+        header("Location: clinetLogin.php?error=missing_fields");
+        exit;
+    }
+} else {
+    // jey direct maghir post 
+    header("Location:".DOMAIN."clinetLogin.php?error=access_report");
+    exit;
+}
+?>
--- handlers/loginHandler.php
+++ handlers/loginHandler.php
@@ -7,35 +7,37 @@ if (!empty($_POST)) {
     if (isset($_POST['username']) && isset($_POST['password'])) {
         try {
             
-            $stmt = $pdo->prepare("SELECT * FROM clients WHERE cin = ?"); // Protection Contre les SQL injections hekka 3lh n7oto ?
+            $stmt = $pdo->prepare("SELECT * FROM utilisateurs WHERE nom_utilisateur = ?"); // Protection Contre les SQL injections hekka 3lh n7oto ?
             $stmt->execute([$_POST['username']]);
             $user = $stmt->fetch(PDO::FETCH_ASSOC);
             
             // Verify user password and set SESSION
-            if ($user && password_verify($_POST['password'], $user["password"])) {
-                $_SESSION['user_id'] = $user["id_donneur"];
-                header("Location:".DOMAIN."/client/index.php");
+            if ($user && password_verify($_POST['password'], $user["mot_de_passe"])) {
+                $_SESSION['user_id'] = $user["id_utilisateur"];
+                $_SESSION['user_role'] = $user["role"] ; 
+                $_SESSION['centre_id'] = $user["id_centre"] ;
+                header("Location:".DOMAIN.strtolower($user['role'])."/index.php");
                 exit;
             } else {
                 // Creds Ghalta 
-               header("Location:".DOMAIN."clinetLogin.php?error=invalid_credentials");
+               header("Location:".DOMAIN."login.php?error=invalid_credentials");
                 exit;
             }
             
         } catch (PDOException $e) {
-            header("Location:".DOMAIN."clinetLogin.php?error=database_error");
+            header("Location:".DOMAIN."login.php?error=database_error");
             //echo "ERROR BASE DE DONNNE" ;
             error_log("Database error: " . $e->getMessage());
             exit;
         }
     } else {
         //  username wa ela  password ne9sa
-        header("Location: clinetLogin.php?error=missing_fields");
+        header("Location: login.php?error=missing_fields");
         exit;
     }
 } else {
     // jey direct maghir post 
-    header("Location:".DOMAIN."clinetLogin.php?error=access_report");
+    header("Location:".DOMAIN."login.php?error=access_report");
     exit;
 }
 ?>
